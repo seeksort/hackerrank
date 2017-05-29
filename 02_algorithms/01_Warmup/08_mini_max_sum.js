712bcc1b892fc2bda2736c1cb50cc8a5a6766e35
Kristin Faner <kfaner@gmail.com>
2017-05-28 19:15:59 -0500

    added using HackerRank solution's approach

--- 02_algorithms/01_Warmup/08_mini_max_sum.js
+++ 02_algorithms/01_Warmup/08_mini_max_sum.js
@@ -9,4 +9,14 @@ const arr = [4, 51, 2, 35, 9999];
   result += sortedArr.slice(0,4).reduce((acc, val) => (acc + val), 0);
   result += ' ' + sortedArr.slice(1,5).reduce((acc, val) => (acc + val), 0);
   console.log(result);
+})();
+
+// I liked HackerRank's solution, and tried to implement using JS native methods:
+(function() {
+  let result = '';
+  const sum = arr.reduce((acc, val) => (acc + val), 0);
+  const max = Math.max(...arr);
+  const min = Math.min(...arr);
+  result = (sum - max) + ' ' + (sum - min);
+  console.log(result);
 })()
