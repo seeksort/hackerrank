4e0ce40bc9b5cbfe9f76fef05c3efb701fa94af9
Kristin Faner <kfaner@gmail.com>
2017-06-10 12:50:10 -0500

    apple and orange

---
+++ 02_algorithms/02_Implementation/02_apple_and_orange.js
@@ -0,0 +1,50 @@
+// https://www.hackerrank.com/challenges/apple-and-orange
+// A house is between two trees, on the same axis. Given positions of the trees and house and fruit that falls from both trees, determine how many trees have fallen onto the house.
+
+// House
+const s = 7;
+const t = 11;
+
+// Trees
+const a = 5;
+const b = 15;
+
+// # Apples
+const m = 3;
+const appleDists = [-2, 2, 1];
+
+// # Oranges
+const n = 2;
+const orangeDists = [5, -6];
+
+/*
+Output (2 lines):
+# apples that fell on the house
+# oranges that fell on the house
+*/
+
+(function() {
+  function fruitPositions (treePosition, distances) {
+    let positions = [];
+    distances.forEach(dist => {
+      positions.push(treePosition + dist);
+    });
+    return positions;
+  }
+
+  const applePositions = fruitPositions(a, appleDists);
+  const orangePositions = fruitPositions(b, orangeDists);
+
+  function fruitOnHouse (positions) {
+    let count = 0;
+    positions.forEach(position => {
+      if (position >= s && position <= t) {
+        count++;
+      }
+    });
+    return count;
+  }
+
+  console.log(output = `${fruitOnHouse(applePositions)}\n${fruitOnHouse(orangePositions)}`);
+  
+})()
